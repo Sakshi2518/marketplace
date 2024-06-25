3d3d83ba4cb50c09169928951aae263a6be6e025
Srishneet Kaur <dsrishneetkaur@gmail.com>
2024-06-25 15:22:40 +0530

    Updated

--- src/components/Home_page/Header.js
+++ src/components/Home_page/Header.js
@@ -1,28 +1,33 @@
 import React from "react"
 import user from "../../images/user.svg"
 import { Link } from "react-router-dom";
-
+import { FaShoppingCart } from "react-icons/fa"; // me
 
 
 export default function Header(){
-    return(
-    <nav className="navbar">
-        <a href="home-section"><h3>MARKETPLACE</h3></a>
-    
+    return (
+      <nav className="navbar">
+        <a href="home-section">
+          <h3>MARKETPLACE</h3>
+        </a>
 
-    <div className="navbar-heading">
-    <div className="explore">
-     <Link to="/shop"><span>Shop</span></Link>
-    </div>
-    <div className="about">
-        <a href="#about-section"><span>About</span></a>
-    </div>
-    <div className="contact">
-        <a href="#contact-section"><span>Contact</span></a>
-    </div>
-    </div>
-  
-    
+        <div className="navbar-heading">
+          <div className="explore">
+            <Link to="/shop">
+              <span>Shop</span>
+            </Link>
+          </div>
+          <div className="about">
+            <a href="#about-section">
+              <span>About</span>
+            </a>
+          </div>
+          <div className="contact">
+            <a href="#contact-section">
+              <span>Contact</span>
+            </a>
+          </div>
+        </div>
 
         {/* <div className="nav-search">
         <input type="text" id="searchbox" value="Search for an item"></input>
@@ -30,14 +35,12 @@ export default function Header(){
         </div> */}
 
         <div className="nav-icons">
-        <i className="fa fa-shopping-cart fa-lg"></i>
-        <img src={user} alt="user-logo" />
+          <Link to="./components/Shop_page/Cart" className="cart-link">
+            <FaShoppingCart size={24} /> </Link> 
+          {/* <i className="fa fa-shopping-cart fa-lg"></i> */}
+          <img src={user} alt="user-logo" />
         </div>
-        
-        
-    </nav>
-            
-          
-    ) 
+      </nav>
+    ); 
     
 }
